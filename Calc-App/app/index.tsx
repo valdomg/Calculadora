077f9284cee64372b32e0179c482258fc99b34f3
valdemiro <valdemiro128@outlook.com>
2024-12-09 09:56:59 -0300

    Switch theme

--- Calc-App/app/index.tsx
+++ Calc-App/app/index.tsx
@@ -1,15 +1,24 @@
-import { Text, View } from "react-native";
-
+import { useState } from "react";
+import { Text, View, StyleSheet, Switch} from "react-native";
+import { ThemeContext } from "./context/ThemeContext";
 
 export default function Index() {
+  const [theme, setTheme] = useState('light');
   return (
     <View
+      
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",        
       }}
     >
+      
+      <Switch
+        value={theme==='light'}
+        onValueChange={() => setTheme(theme==='light'? 'dark':'light')}
+      />
+
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
