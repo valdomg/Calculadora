c0e65113243155c1e34cd54702da8322ba836db3
valdemiro <valdemiro128@outlook.com>
2024-12-20 14:30:28 -0300

    Erros nos nomes de cores consertado

--- Calc-App/app/styles/Colors.tsx
+++ Calc-App/app/styles/Colors.tsx
@@ -1,11 +1,11 @@
 export const myColors = {
     light:'#F1F2F3',
-    dark: '17171C',
+    dark: '#17171C',
     blue: '#4B5EFC',
     btnGray: '#4E505F',
     btnDark: '#2E2F38',
     gray: '#747477',
     black: '#OOOOO',
-    white: 'FFFFFF',
-    result: '46D5B2',
+    white: '#FFFFFF',
+    result: '#46D5B2',
 }
